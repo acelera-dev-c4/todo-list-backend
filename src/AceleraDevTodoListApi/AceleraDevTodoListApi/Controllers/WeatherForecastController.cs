cb0294c123eb78ead980e178c3a2089c799cdbb9
Maurício <mauricio.mafrapedreiro@gmail.com>
2024-05-14 17:00:42 -0300

    Teste

--- src/AceleraDevTodoListApi/AceleraDevTodoListApi/Controllers/WeatherForecastController.cs
+++ src/AceleraDevTodoListApi/AceleraDevTodoListApi/Controllers/WeatherForecastController.cs
@@ -18,12 +18,15 @@ namespace AceleraDevTodoListApi.Controllers
             _logger = logger;
         }
 
+
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+
+                newDate = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
