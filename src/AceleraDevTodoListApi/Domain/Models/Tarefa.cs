5c0a1d533eadb7c85229d0f3ea9efd27467f3c54
czuy <weslleyvitorbatista@gmail.com>
2024-05-14 17:45:43 -0300

    corrigido arquivos

--- src/AceleraDevTodoListApi/Domain/Models/Tarefa.cs
+++ src/AceleraDevTodoListApi/Domain/Models/Tarefa.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domain.Entitys;
+﻿namespace Domain.Entitys;
 public class Tarefa
 {
     public int Id { get; set; }
