95f0702f89864008e4c86cd7ea2da64b5dbcb85c
Maurício <mauricio.mafrapedreiro@gmail.com>
2024-05-15 16:53:52 -0300

    [FIX] Incluido Services como projeto separado.

--- src/AceleraDevTodoListApi/AceleraDevTodoListApi/Services/SubTarefaService.cs
+++ src/AceleraDevTodoListApi/AceleraDevTodoListApi/Services/SubTarefaService.cs
@@ -1,39 +1,38 @@
-﻿namespace AceleraDevTodoListApi.Services
+﻿namespace AceleraDevTodoListApi.Services;
+
+public class SubTarefaService
 {
-    public class SubTarefaService
+    private readonly ISubtarefaRepository _subtarefaRepository;
+    public SubTarefaService(IUSubtarefaRepository subtarefaRepository) {
+        _subtarefaRepository = subtarefaRepository;
+    }
+
+    public SubTarefa Create(SubTarefa subtarefa)
     {
-        private readonly ISubtarefaRepository _subtarefaRepository;
-        public SubTarefaService(IUSubtarefaRepository subtarefaRepository) {
-            _subtarefaRepository = subtarefaRepository;
-        }
+        return _subtarefaRepository.Create(subtarefa);
+    }
 
-        public SubTarefa Create(SubTarefa subtarefa)
-        {
-            return _subtarefaRepository.Create(subtarefa);
-        }
+    public void Delete(int id)
+    {
+        _subtarefaRepository.Delete(id);
+    }
 
-        public void Delete(int id)
-        {
-            _subtarefaRepository.Delete(id);
-        }
+    public SubTarefa? GetById(int id)
+    {
+        return _subtarefaRepository.GetById(id);
+    }
 
-        public SubTarefa? GetById(int id)
-        {
-            return _subtarefaRepository.GetById(id);
-        }
-
-        public List<SubTarefa> List()
-        {
-            var subtarefas = _subtarefaRepository.GetAll();
-            return subtarefas;
-
-        }
-
-        public SubTarefa Update(SubTarefa subtarefaUpdate, int id)
-        {
-            return _subtarefaRepository.Update(subtarefaUpdate, id);
-        }
-
+    public List<SubTarefa> List()
+    {
+        var subtarefas = _subtarefaRepository.GetAll();
+        return subtarefas;
 
     }
+
+    public SubTarefa Update(SubTarefa subtarefaUpdate, int id)
+    {
+        return _subtarefaRepository.Update(subtarefaUpdate, id);
+    }
+
+
 }
--- src/AceleraDevTodoListApi/AceleraDevTodoListApi/Services/TarefaService.cs
+++ src/AceleraDevTodoListApi/AceleraDevTodoListApi/Services/TarefaService.cs
@@ -1,37 +1,36 @@
-﻿namespace AceleraDevTodoListApi.Services
+﻿namespace AceleraDevTodoListApi.Services;
+
+public class TarefaService
 {
-    public class TarefaService
+    private readonly ITarefaRepository _tarefaRepository;
+
+    public TarefaService(ITarefaRepository tarefaRepository) {
+        _tarefaRepository = tarefaRepository;
+    }
+
+
+    public Tarefa Create(Tarefa tarefa)
     {
-        private readonly TarefaRepository _tarefaRepository;
+        return _tarefaRepository.Create(tarefa);
+    }
 
-        public TarefaService(TarefaRepository tarefaRepository) {
-            _tarefaRepository = tarefaRepository;
-        }
+    public void Delete(int id)
+    {
+        _tarefaRepository.Delete(id);
+    }
 
+    public Tarefa? GetById(int id)
+    {
+        return _tarefaRepository.GetById(id);
+    }
 
-        public Tarefa Create(Tarefa tarefa)
-        {
-            return _tarefaRepository.Create(tarefa);
-        }
+    public List<Tarefa> List()
+    {
+        return _tarefaRepository.GetAll();
+    }
 
-        public void Delete(int id)
-        {
-            _tarefaRepository.Delete(id);
-        }
-
-        public Tarefa? GetById(int id)
-        {
-            return _tarefaRepository.GetById(id);
-        }
-
-        public List<Tarefa> List()
-        {
-            return _tarefaRepository.GetAll();
-        }
-
-        public Tarefa Update(Tarefa tarefa, int id)
-        {
-            return _tarefaRepository.Update(tarefa, id);
-        }
+    public Tarefa Update(Tarefa tarefa, int id)
+    {
+        return _tarefaRepository.Update(tarefa, id);
     }
 }
