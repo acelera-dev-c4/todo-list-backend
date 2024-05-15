50f4ac031d6610b793c643ba5cee5db3237331b5
GhortheBrute <eitaro1@gmail.com>
2024-05-15 17:06:21 -0300

    Criacao de Jesus2.

---
+++ src/Api/Controllers/UsuarioController.cs
@@ -0,0 +1,33 @@
+﻿using AceleraDevTodoListApi.DB;
+using Domain.Entitys;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsuarioController : ControllerBase
+{
+    private readonly MyDBContext _context;
+
+    public UsuarioController(MyDBContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost("Cadastro")]
+    public IActionResult Post(Usuario usuario)
+    {
+        try
+        {
+            var newUser = _context.Usuarios.Add(usuario);
+            _context.SaveChanges();
+            return Ok(newUser);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest($"Falha no cadastro. {ex.Message}");
+        }
+        
+    }
+}
