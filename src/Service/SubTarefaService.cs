164106e20f874fef3ae0c14477c8dc0ec826e5eb
Maurício <mauricio.mafrapedreiro@gmail.com>
2024-05-16 21:30:38 -0300

    Incluída camada Service.

---
+++ src/Service/SubTarefaService.cs
@@ -0,0 +1,41 @@
+﻿using AceleraDevTodoListApi.Infra.Repositories;
+using Domain.Entitys;
+
+namespace AceleraDevTodoListApi.Services;
+
+public class SubTarefaService
+{
+    private readonly ISubTarefaRepository _subTarefaRepository;
+    public SubTarefaService(ISubTarefaRepository subTarefaRepository) {
+        _subTarefaRepository = subTarefaRepository;
+    }
+
+    public SubTarefa Create(SubTarefa subtarefa)
+    {
+        return _subTarefaRepository.Create(subtarefa);
+    }
+
+    public void Delete(int id)
+    {
+        _subTarefaRepository.Delete(id);
+    }
+
+    public SubTarefa? GetById(int id)
+    {
+        return _subTarefaRepository.GetById(id);
+    }
+
+    public List<SubTarefa> List()
+    {
+        var subTarefas = _subTarefaRepository.GetAll();
+        return subTarefas;
+
+    }
+
+    public SubTarefa Update(SubTarefa subtarefaUpdate, int id)
+    {
+        return _subTarefaRepository.Update(subtarefaUpdate, id);
+    }
+
+
+}
