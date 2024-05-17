b68212061ec4b02028cba390e56e1e6735828543
Maurício <mauricio.mafrapedreiro@gmail.com>
2024-05-16 22:28:10 -0300

    Incluído Middleware de tratamento de exceções.

---
+++ src/Api/Middlewares/ExceptionHandler.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.Json;
+using Newtonsoft.Json;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Api.Middlewares
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class ExceptionHandler
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandler(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext httpContext, Exception ex)
+        {
+            var code = HttpStatusCode.InternalServerError;
+
+            if (ex is DbUpdateException) { 
+                code = HttpStatusCode.InternalServerError;
+            }
+
+            if (ex is DbUpdateConcurrencyException) { 
+                code = HttpStatusCode.Conflict;
+            }
+
+            if (ex is ArgumentNullException)
+            {
+                code = HttpStatusCode.BadRequest;
+            }
+
+            var result = JsonConvert.SerializeObject(new { error = ex.Message });
+
+            return httpContext.Response.WriteAsync(result);
+        }
+
+    }
+
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class ExceptionHandlerExtensions
+    {
+        public static IApplicationBuilder UseExceptionHandler(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionHandler>();
+        }
+    }
+}
