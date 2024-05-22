71bbd1aa808551a1865e6c2b8df3a5bbb4d04fb4
Weslley <weslleyvitorbatista@gmail.com>
2024-05-22 16:48:19 -0300

    [REFACTOR] Removido arquivos duplicados, removido user-secrets.

--- src/Domain/Models/SubTask2.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Domain.Models;
-public class SubTask2
-{
-    public int Id { get; set; }
-    public int MainTaskId { get; set; }
-    public string? Description { get; set; }
-    public bool Finished { get; set; } = false;
-}
