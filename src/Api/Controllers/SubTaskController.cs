d57b50f4624997294fe75dcc7e9cfccfd840821d
PauloEwerson <ewersonmrt@gmail.com>
2024-05-22 20:13:43 -0300

    feat: Adiciona [Authorize] para proteger todos os endpoints do SubTaskController com autenticação JWT.

--- src/Api/Controllers/SubTaskController.cs
+++ src/Api/Controllers/SubTaskController.cs
@@ -1,71 +1,74 @@
 ﻿using Infra.DB;
 using Microsoft.AspNetCore.Mvc;
 using Domain.Models;
+using Microsoft.AspNetCore.Authorization;
 
-namespace Api.Controllers;
-
-[ApiController]
-[Route("[controller]")]
-public class SubTaskController : ControllerBase
+namespace Api.Controllers
 {
-    private readonly MyDBContext _myDBContext;
-
-    public SubTaskController(MyDBContext myDBContext)
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize]
+    public class SubTaskController : ControllerBase
     {
-        _myDBContext = myDBContext;
-    }
+        private readonly MyDBContext _myDBContext;
 
-    [HttpGet]
-    public IActionResult Get()
-    {
-        return Ok(_myDBContext.SubTasks.ToList());
-    }
-
-    [HttpGet("{subTaskId}")]
-    public IActionResult Get(int subTaskId)
-    {
-        var subTask = _myDBContext.SubTasks.Find(subTaskId);
-        return subTask is null ? NotFound() : Ok(subTask);
-    }
-
-    [HttpPost]
-    public IActionResult Post([FromBody] SubTask newSubTask)
-    {
-        _myDBContext.SubTasks.Add(newSubTask);
-        _myDBContext.SaveChanges();
-        return Ok(newSubTask);
-    }
-
-    [HttpPut("{subTaskId}")]
-    public IActionResult Put(int subTaskId, [FromBody] string updateDescription)
-    {
-        var subTask = _myDBContext.SubTasks.Find(subTaskId);
-        if (subTask is null)
+        public SubTaskController(MyDBContext myDBContext)
         {
-            return NotFound($"Subtarefa não encontrada.");
+            _myDBContext = myDBContext;
         }
 
-        subTask.Description = updateDescription;
-
-        _myDBContext.Update(subTask);
-        _myDBContext.SaveChanges();
-
-        return Ok(subTask);
-    }
-
-    [HttpDelete("{subTaskId}")]
-    public IActionResult Delete([FromRoute] int subTaskId)
-    {
-        var subTask = _myDBContext.SubTasks.Find(subTaskId);
-
-        if (subTask is null)
+        [HttpGet]
+        public IActionResult Get()
         {
-            return NotFound($"SubTarefa não encontrada.");
+            return Ok(_myDBContext.SubTasks.ToList());
         }
 
-        _myDBContext.Remove(subTask);
-        _myDBContext.SaveChanges();
+        [HttpGet("{subTaskId}")]
+        public IActionResult Get(int subTaskId)
+        {
+            var subTask = _myDBContext.SubTasks.Find(subTaskId);
+            return subTask is null ? NotFound() : Ok(subTask);
+        }
 
-        return NoContent();
+        [HttpPost]
+        public IActionResult Post([FromBody] SubTask newSubTask)
+        {
+            _myDBContext.SubTasks.Add(newSubTask);
+            _myDBContext.SaveChanges();
+            return Ok(newSubTask);
+        }
+
+        [HttpPut("{subTaskId}")]
+        public IActionResult Put(int subTaskId, [FromBody] string updateDescription)
+        {
+            var subTask = _myDBContext.SubTasks.Find(subTaskId);
+            if (subTask is null)
+            {
+                return NotFound("Subtarefa não encontrada.");
+            }
+
+            subTask.Description = updateDescription;
+
+            _myDBContext.Update(subTask);
+            _myDBContext.SaveChanges();
+
+            return Ok(subTask);
+        }
+
+        [HttpDelete("{subTaskId}")]
+        public IActionResult Delete([FromRoute] int subTaskId)
+        {
+            var subTask = _myDBContext.SubTasks.Find(subTaskId);
+
+            if (subTask is null)
+            {
+                return NotFound("SubTarefa não encontrada.");
+            }
+
+            _myDBContext.Remove(subTask);
+            _myDBContext.SaveChanges();
+
+            return NoContent();
+        }
     }
 }
