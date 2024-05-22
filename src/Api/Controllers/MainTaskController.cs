9fe17a418e92fd956571a6dc385bf39fe2677b34
Maurício Mafra Pedreiro <mauricio_mafra_pedreiro@atacadao.com.br>
2024-05-22 17:34:17 -0300

    [FIX 2/2] Feita tradução dos controllers para inglês.

---
+++ src/Api/Controllers/MainTaskController.cs
@@ -0,0 +1,61 @@
+﻿using Domain.Mappers;
+using Domain.Request;
+using Microsoft.AspNetCore.Mvc;
+using Service;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MainTaskController : ControllerBase
+{
+    private readonly IMainTaskService _mainTaskService;
+
+    public MainTaskController(IMainTaskService mainTaskService)
+    {
+        _mainTaskService = mainTaskService;
+    }   
+
+    [HttpGet("{userId}")]
+    public IActionResult Get([FromRoute]int userId)
+    {
+        var mainTasks = _mainTaskService.Get(userId);
+        return mainTasks is null ? NotFound() : Ok(mainTasks);
+    }
+
+    [HttpPost]
+    public IActionResult Post([FromBody] RequisicaoMainTask requisicaoMainTask)
+    {
+        var novaMainTask = MapeadorMainTask.ParaClasse(requisicaoMainTask);
+        _mainTaskService.Create(novaMainTask);
+        return Created();
+    }
+
+    [HttpPut("{mainTaskId}")]
+    public IActionResult Put(int mainTaskId, [FromBody] MainTaskUpdateRequest updatedDescription)
+    {
+        var mainTask = _mainTaskService.Find(mainTaskId);
+
+        if (mainTask is null)
+            return NotFound($"MainTask não encontrada.");
+
+        mainTask.Descricao = updatedDescription.Description;
+
+        if (mainTask.Id is not null)
+            _mainTaskService.Update(mainTask, (int)mainTask.Id);
+
+        return Ok(mainTask);
+    }
+
+    [HttpDelete("{mainTaskId}")]
+    public IActionResult Delete(int mainTaskId)
+    {
+        var mainTask = _mainTaskService.Find(mainTaskId);
+        if (mainTask is null)
+            return NotFound($"MainTask não encontrada.");
+
+        _mainTaskService.Delete(mainTaskId);
+
+        return NoContent();
+    }
+}
---
+++ src/Api/Controllers/SubTaskController.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Models;
+using Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SubTaskController : Controller
+{
+    private readonly ISubTaskService _mainTaskService;
+
+    public SubTaskController(ISubTaskService mainTaskService)
+    {
+        _mainTaskService = mainTaskService;
+    }
+
+    [HttpGet("{mainTaskId}")]
+    public IActionResult Get([FromRoute] int mainTaskId)
+    {
+        var mainTasks = _mainTaskService.List(mainTaskId);
+        return mainTasks is null ? NotFound() : Ok(mainTasks);
+    }
+
+    [HttpPost]
+    public IActionResult Post([FromBody] SubTask novaSubTask)
+    {
+        _mainTaskService.Create(novaSubTask);
+        return Ok(novaSubTask);
+    }
+
+    [HttpPut("{subTaskId}")]
+    public IActionResult Put([FromRoute] int subTaskId, [FromBody] SubTask updatedSubTask)
+    {
+        _mainTaskService.Update(updatedSubTask, subTaskId);
+
+        return Ok(updatedSubTask);
+    }
+
+    [HttpDelete("{subTaskId}")]
+    public IActionResult Delete([FromRoute] int subTaskId)
+    {
+        _mainTaskService.Delete(subTaskId);
+
+        return NoContent();
+    }
+}
