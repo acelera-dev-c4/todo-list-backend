d37f345f7dfbbdd459e972fbf238f46867f18c22
LeandroMPio <leandropio@gmail.com>
2024-06-13 20:55:33 -0300

    feat: add Notification and Subscription Models, update seed and added Notification and Subscription migration

---
+++ src/Infra/Migrations/20240613233439_Add_Subscription_And_Notification_Table.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Subscription_And_Notification_Table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Subscriptions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SubTaskIdSubscriber = table.Column<int>(type: "int", nullable: false),
+                    MainTaskIdTopic = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subscriptions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Subscriptions_MainTasks_MainTaskIdTopic",
+                        column: x => x.MainTaskIdTopic,
+                        principalTable: "MainTasks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Subscriptions_SubTasks_SubTaskIdSubscriber",
+                        column: x => x.SubTaskIdSubscriber,
+                        principalTable: "SubTasks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Notifications",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SubscriptionId = table.Column<int>(type: "int", nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Readed = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notifications", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Notifications_Subscriptions_SubscriptionId",
+                        column: x => x.SubscriptionId,
+                        principalTable: "Subscriptions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notifications_SubscriptionId",
+                table: "Notifications",
+                column: "SubscriptionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subscriptions_MainTaskIdTopic",
+                table: "Subscriptions",
+                column: "MainTaskIdTopic");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subscriptions_SubTaskIdSubscriber",
+                table: "Subscriptions",
+                column: "SubTaskIdSubscriber");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notifications");
+
+            migrationBuilder.DropTable(
+                name: "Subscriptions");
+        }
+    }
+}
