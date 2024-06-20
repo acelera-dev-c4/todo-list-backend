51880096b2392dfaccedeaa357d4c5703da87718
Vagner1212 <corvonegro222@gmail.com>
2024-06-20 15:56:52 -0300

    Notificação de uma Subtarefa se tem Subscrição

---
+++ src/Infra/HttpClient/HttpClient.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Infra;
+    public class NotificationHttpClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public NotificationHttpClient()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:7056/"); 
+        }
+
+        public async Task<bool> CheckSubtaskSubscriptionAsync(string subtaskId)
+        {
+            var url = $"subscriptions/{subtaskId}";
+            var response = await _httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+              
+                return content.Contains("assinada", StringComparison.OrdinalIgnoreCase); 
+            }
+            else if (response.StatusCode == HttpStatusCode.BadRequest)
+            {
+                throw new ArgumentException("Subtask ID inválido.");
+            }
+            else
+            {
+                throw new HttpRequestException($"Erro ao acessar a API de Notification. Status code: {response.StatusCode}");
+            }
+        }
+    }
+
